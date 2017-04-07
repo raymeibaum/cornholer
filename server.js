61fdda63ec2017a658d89e438126e5d6eb7ec7d7
Ray Meibaum <rmeibaum@me.com>
2017-04-07 12:09:16 -0400

    create static file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+
 app.listen(process.env.PORT || 3000, function (){
   console.log('Project 3 server now listening on port 3000 dammmm!!');
 });
