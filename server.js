0bd0d0d00aac1e181bf65a2d6ae30ef2ebd08aad
Ray Meibaum <rmeibaum@me.com>
2017-04-09 11:28:45 -0400

    add dependencies

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 require('dotenv').config();
 
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({ secret: 'cornholeforever', resave: true, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
