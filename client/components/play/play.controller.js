2ac3cf22571c4efd7ca096afb1b33f1d58fee5e9
Ray Meibaum <rmeibaum@me.com>
2017-04-11 10:22:05 -0400

    start signup

--- client/components/play/play.controller.js
+++ client/components/play/play.controller.js
@@ -1,5 +1,32 @@
-function PlayController() {
+PlayController.$inject = ['$rootScope', 'UsersService', 'SocketService']
+
+function PlayController($rootScope, UsersService, SocketService) {
   const vm = this;
+
+  vm.users = [];
+  vm.currentUser = $rootScope.currentUser;
+  vm.signup = {
+    user1: vm.currentUser
+  }
+  vm.submitSignUp = submitSignUp;
+  vm.list = [];
+
+  activate()
+
+  $rootScope.$watchCollection('list', function(updatedList) {
+    vm.list = updatedList;
+  });
+
+  function activate() {
+    UsersService
+      .loadAll()
+      .then(function setUsers(response) {
+        vm.users = response.data.users;
+      })
+  }
+  function submitSignUp() {
+    // SocketService.
+  }
 }
 
 module.exports = PlayController;
